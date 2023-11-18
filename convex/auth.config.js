59863f6bfd06c05a5646564441472e0447d0942e
Ambro-Dev <piotr.ambroziak@ambro.dev>
2023-11-18 10:23:04 +0100

    convex update

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,8 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://apparent-katydid-85.clerk.accounts.dev",
       applicationID: "convex",
-    }
-  ]
-}
+    },
+  ],
+};
